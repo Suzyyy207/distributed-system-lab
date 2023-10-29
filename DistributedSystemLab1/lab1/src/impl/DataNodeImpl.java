bff8a01bf0dd135734a4acf7ab86c8468a0f640e
suzyyy <suzyyy207@gmail.com>
2023-10-29 10:58:31 +0800

    DataNode: DataNode() ande read code done

--- DistributedSystemLab1/lab1/src/impl/DataNodeImpl.java
+++ DistributedSystemLab1/lab1/src/impl/DataNodeImpl.java
@@ -3,11 +3,36 @@ package impl;
 import api.DataNodePOA;
 
 public class DataNodeImpl extends DataNodePOA {
-    private List<octet> blocks;
+    private List<byteArray> blocks;
+
+    public DataNodeImpl(node_id){
+        this.blocks = new ArrayList<>();
+
+        try (FileInputStream file = new FileInputStream("../DataNodeFile/DataNode"+node_id+".dat")) {
+            int bytesRead = 4*1024;
+            byte[] block = new byte[bytesRead];
+
+            // 从文件中读取数据到buffer，直到文件末尾
+            while ((bytesRead = file.read(buffer)) != -1) {
+                // 创建新的byte数组，将读取的数据放入其中
+                byte[] block = new byte[bytesRead];
+                System.arraycopy(buffer, 0, block, 0, bytesRead);
+
+                // 将octet数组添加到blocks列表中
+                this.blocks.add(block);
+            }
+
+            System.out.println("DataNode "+node_id+" read done");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("DataNoe"+node_id+" read error");
+        }
+
+    }
 
     @Override
     public byte[] read(int block_id){
-        octet data;
+        byteArray data;
         data = blocks.get(block_id);
         return data;
     }
@@ -19,6 +44,7 @@ public class DataNodeImpl extends DataNodePOA {
 
     @Override
     public int randomBlockId() {
+
         return 0;
     }
 }
