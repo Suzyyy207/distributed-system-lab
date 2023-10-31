968161ce64893979d2ab8b9d572ed2442fea4db4
suzyyy <suzyyy207@gmail.com>
2023-10-31 17:41:44 +0800

    Client: open & close code done

--- DistributedSystemLab1/lab1/src/impl/ClientImpl.java
+++ DistributedSystemLab1/lab1/src/impl/ClientImpl.java
@@ -2,10 +2,27 @@ package impl;
 //TODO: your implementation
 import api.Client;
 public class ClientImpl implements Client{
+    private NameNode name_node;
+    private DataNode[] data_nodes = new DataNode[MAX_DATA_NODE];
+    private List<FileDesc> my_files;
+    private HashMap<int, FileDesc> fd_file;
 
+    //设定：map的时候将新文件的fd map为现在文件的长度+1
     @Override
     public int open(String filepath, int mode) {
-        return 0;
+        String meta_data_str;
+        meta_data_str = this.name_node.open(filepath, mode);
+        if (meta_data_str == null){
+            return -1;
+        }
+
+        FileDesc file = new FileDesc();
+        file = file.fromString(meta_data_str);
+        this.my_files.add(file);
+        int fd = this.fd_file.size() + 1;
+        this.fd_file.put(fd, file);
+
+        return fd;
     }
 
     @Override
@@ -20,6 +37,10 @@ public class ClientImpl implements Client{
 
     @Override
     public void close(int fd) {
-
+        FileDesc file = this.fd_file.get(fd);
+        String filepath = file.getFilepath();
+        this.name_node.close(file);
+        this.fd_file.remove(fd);
+        this.my_files.remove(file);
     }
 }
